b1873092acad6dd0c2e92d302cc15f0ce5847f51
gustavorosacorrea <gustavo.rosacorrea@hotmail.com>
2024-08-21 21:08:15 -0300

    Update music-list.js

--- musicas/javascript/music-list.js
+++ musicas/javascript/music-list.js
@@ -1,14 +1,1 @@
-let allMusic = [
-    {
-        name: "Juntos Construindo uma Nova História",
-        artist: "Gustavo Rosa Corrêa",
-        img: "jcunh",
-        src: "jcunh"
-    },
-    {
-	name: "Eu Vou Votar Elise e Welter",
-	artist: "Sinara e Gustavo",
-	img: "jcunh",
-	src: "Eu-vou-votar_-_Sinara-e-Gustavo"
-    }   
-]
+
